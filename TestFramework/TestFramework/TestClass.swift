7243d8fad4d66baa9c80ab9c6d9e5007d9c834b3
Richie Shilton <richie.shilton@bilue.com.au>
2021-04-26 12:25:29 +1000

    initial framework

---
+++ TestFramework/TestFramework/TestClass.swift
@@ -0,0 +1,17 @@
+//
+//  TestClass.swift
+//  TestFramework
+//
+//  Created by Richie Shilton on 26/4/21.
+//
+
+import Foundation
+
+public class TestClass {
+    
+    public init() { }
+    
+    public func test() {
+        print("I'm working yay!")
+    }
+}
